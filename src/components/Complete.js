1c0d18abd6ac8094d8ef919f60c40f75e2d4f4dc
yU-kiki <yuki521ike@gmail.com>
2022-10-22 03:05:12 +0900

    isGenerateの初期値をfalseに変更

--- src/components/Complete.js
+++ src/components/Complete.js
@@ -2,7 +2,7 @@ import { React, useState } from "react";
 import { Link } from "react-router-dom";
 
 const Complete = () => {
-  const [isGenerate, setGenerate] = useState(true);
+  const [isGenerate, setGenerate] = useState(false);
   return (
     <>
       {isGenerate || (
