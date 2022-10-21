c71f11241bb5620d36ac0c4766a16a5736f1b427
yU-kiki <yuki521ike@gmail.com>
2022-10-21 23:38:25 +0900

    コンプリート画面作成

--- src/components/Complete.js
+++ src/components/Complete.js
@@ -1,9 +1,25 @@
-import React from "react";
+import { React, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Complete = () => {
+  const [isGenerate, setGenerate] = useState(true);
   return (
     <>
-      <h1>Hello React From Complete</h1>
+      {isGenerate || (
+        <>
+          <div className="loader-area">
+            <p className="fs24">generating...</p>
+            <div class="loader">Loading...</div>
+          </div>
+        </>  
+      )}
+      {isGenerate && (
+        <>
+          <Link onClick={() => setGenerate(false)} to={`/complete/`} className='btn btn-create'>
+            <p>再作成する</p>
+          </Link>
+        </>
+      )}
     </>
   );
 }
