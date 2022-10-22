9c71bf872d0c26f8eb3e8ef35dfc73b4a357be8f
yU-kiki <yuki521ike@gmail.com>
2022-10-22 10:10:17 +0900

    一旦パス

--- src/components/Complete.js
+++ src/components/Complete.js
@@ -1,13 +1,25 @@
-import { React, useState } from "react";
+import { React, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const Complete = () => {
   const [isGenerate, setGenerate] = useState(false);
+  const[posts, setPosts] = useState([])
+
+  useEffect(() => {
+    fetch('https://google.com', { method: 'GET' })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setPosts(data)
+      })
+  }, [])
+
   return (
     <>
       {isGenerate || (
         <>
           <div className="loader-area">
+            <p>{posts}</p>
             <p className="fs24">generating...</p>
             <div class="loader">Loading...</div>
           </div>
